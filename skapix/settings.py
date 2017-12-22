a6d38a8e0f6f88ff0222ef966331c4736983af0a
zer0dev <zer0nerd@protonmail.com>
2017-12-22 14:36:27 -0800

    test4

--- skapix/settings.py
+++ skapix/settings.py
@@ -116,12 +116,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-#    os.path.join(PROJECT_ROOT, 'static'),
+#    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 AUTHENTICATION_BACKENDS = ('skapix.authb.AuthB',)
