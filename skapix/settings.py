eadb198ec3983de14032a90ac0dbcc174f68b3c4
zer0dev <zer0nerd@protonmail.com>
2017-12-23 10:50:03 -0800

    Inner-Log-in-fixed

--- skapix/settings.py
+++ skapix/settings.py
@@ -27,7 +27,7 @@ STATICFILES_DIRS = (
 SECRET_KEY = 'nvqwd0(ptahogm2d*j4j1fkgk2gi!v07$f7go!m!k_jf3p*9jp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'skapix.herokuapp.com', '127.0.0.1']
 
