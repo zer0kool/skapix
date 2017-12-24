fa975de226e46a32102af867f9fbcf055fcaa934
zer0dev <zer0nerd@protonmail.com>
2017-12-24 09:24:25 -0800

    new DB

--- skapix/settings.py
+++ skapix/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'nvqwd0(ptahogm2d*j4j1fkgk2gi!v07$f7go!m!k_jf3p*9jp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'skapix.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['skapix.herokuapp.com']
 
 
 # Application definition
@@ -83,6 +83,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 # Password validation
