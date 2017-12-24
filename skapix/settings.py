d61ae61baf129a00b1b3f63cf5b45fab1a8bfe69
zer0dev <zer0nerd@protonmail.com>
2017-12-24 10:55:12 -0800

    remove import-heroku

--- skapix/settings.py
+++ skapix/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -156,4 +156,4 @@ SECURE_HSTS_SECONDS = True
 SECURE_FRAME_DENY = True
 
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
