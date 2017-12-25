64ab74142c94bcbd8a88ddaa866c5c89ddf641ea
zer0dev <zer0nerd@protonmail.com>
2017-12-25 01:17:26 -0800

    juliet credits

--- skapix/static/assets/js/init.js
+++ skapix/static/assets/js/init.js
@@ -1,3 +1,7 @@
  $(document).ready(function(){
       $('.slider').slider();
     });
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
