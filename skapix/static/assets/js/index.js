a6cdff957fe1531bb83fecd8357b9e41a028f704
zer0dev <dwebcrew@protonmail.ch>
2018-03-18 18:51:53 -0700

    daily update

---
+++ skapix/static/assets/js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+            $('#go').click(function() {
+                $.post("ajax-login", {
+                        email: $("#email").val(),
+                        password: $("#password").val()
+                    },
+                    function(data, status) {
+                        if (JSON.parse(data).Status == 'Success') {
+                            window.location = '';
+                        } else {
+                            $('#errors').html("<span>" + JSON.parse(data).Message + "</span>")
+                            $('#errors').css('display', 'block')
+                            Materialize.toast('Email or password is incorrect.', 4000, 'red')
+                        }
+                    });
+                return false;
+                $('#go').click(function() {
+                    $.post("ajax-login", {
+                            email: $("#email").val(),
+                            password: $("#password").val()
+                        },
+                        function(data, status) {
+                            if (JSON.parse(data).Status == 'Success') {
+                                window.location = '';
+                            } else {
+                                $('#errors').html("<span>" + JSON.parse(data).Message + "</span>")
+                                $('#errors').css('display', 'block')
+                            }
+                        });
+                    return false;
+                })
+            })
+        })
---
+++ skapix/static/assets/js/sign-up.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('#go').click(function () {
+        $.post("ajax-sign-up", {
+                username: $("#username").val(),
+                email: $("#email").val(),
+                password: $("#password").val()
+            },
+            function (data, status) {
+                if (JSON.parse(data).Status == 'Success') {
+                    Materialize.toast('Account Created!', 3000, 'rounded') // 'rounded' is the class I'm applying to the toast
+                    window.location = '/';
+                    Materialize.toast('Please log-in using your credentials', 3000, 'rounded') // 'rounded' is the class I'm applying to the toast
+                } else {
+                    if (JSON.parse(data).Message == "Username already in use.") {
+                        document.getElementsByTagName("i")[0].setAttribute("class", "material-icons prefix red-text");
+                        Materialize.toast('Username already in use.', 4000, 'red')
+                    }
+                    if (JSON.parse(data).Message == "Password must be between 6 and 32 characters long.") {
+                        document.getElementsByTagName("i")[2].setAttribute("class", "material-icons prefix red-text");
+                        document.getElementsByTagName("input")[3].setAttribute("class", "invalid");
+                        Materialize.toast('Password is to weak!', 4000, 'red')
+                    }
+                    $('#errors').html("<span>" + JSON.parse(data).Message + "</span>")
+                    $('#errors').css('display', 'block')
+                }
+            });
+        return false;
+    })
+})
